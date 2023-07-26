ea6b3245905de6af52deac4f83943a7802052420
Grace-Avor <graceavor2021@gmail.com>
2023-07-26 01:03:54 +0100

    update

--- src/javscript.js
+++ src/javscript.js
@@ -1,6 +1,6 @@
 function search(event) {
   if (event.key === "Enter")
-    alert("Thank for searching, We are still working on this app");
+    alert("Oops! Sorry, We are still working on this app");
 }
 var input = document.getElementById("search-input");
 input.addEventListener("keypress", search);
