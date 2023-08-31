6b15d07588a8ee0cf56fd9bed7fd4be3bbe6fdea
Grace-Avor <graceavor2021@gmail.com>
2023-08-31 07:52:57 +0100

    Added forecast API

--- src/javscript.js
+++ src/javscript.js
@@ -1,7 +1,64 @@
+let h2 = document.querySelector("#date");
+
+let now = new Date();
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let day = now.getDay();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+day = days[now.getDay()];
+
+h2.innerHTML = `${day} ${hours}:${minutes}`;
+
+function weatherForecasts(response) {
+  
+  let forecastCall = document.querySelector("#weather-forecasts");
+  //let weekDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecast = response.data.daily; // stored the response forecast in this variable
+  
+  let forecastContent = `<div class="row">`;
+  //replaced the days variale with the forecastResponse variable
+  forecast.forEach(function (forecastDays) {
+    forecastContent =
+      forecastContent +
+      `<div class="col-2">
+            <div class="forecast-days">${forecastDays.dt}</div>
+              <img src="https://openweathermap.org/img/wn/${forecastDays.weather[0].icon}@2x.png" />
+              <div class="forecast-temp">
+                <span class="forecast-temp-max">${forecastDays.temp.max}</span> 
+                <span class="forecast-temp-min">${forecastDays.temp.min}</span>
+            </div>
+      </div>`;
+  });
+
+  forecastContent = forecastContent + `</div>`;
+  forecastCall.innerHTML = forecastContent;
+}
+
+//created the forecast coordinate function. when this function is called it goes into the weatherForecats function and display
+function forecastCoordinates(coords) {
+  let apiKey = "ff69729f5d84be9471536bc122ea91ad";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(weatherForecasts);
+}
+
 function weather(response) {
   let myTemp = document.querySelector("#temp"); // define the call the temp id
   let temperature = Math.round(response.data.main.temp); // round the temp to 2 digit numbers using API call
-  let city = document.querySelector("#city"); // define the city id
+  let cityElement = document.querySelector("#city"); // define the city id
   let cityDescription = document.querySelector("#description");
   let icon = document.querySelector("#icon");
   let humidity = document.querySelector("#humidity");
@@ -9,28 +66,32 @@ function weather(response) {
 
   celsius = Math.round(response.data.main.temp);
 
-  city.innerHTML = `${response.data.name}`; // Assign the API response to city innerHTML
+  cityElement.innerHTML = `${response.data.name}`; // Assign the API response to city innerHTML
   myTemp.innerHTML = `${temperature}`; // Assign the rounded temp from previous
   cityDescription.innerHTML = `${response.data.weather[0].description}`;
+  humidity.innerHTML = `${response.data.main.humidity}%`;
+  speed.innerHTML = Math.round(response.data.wind.speed);
   icon.setAttribute(
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
-  humidity.innerHTML = `${response.data.main.humidity}%`;
-  speed.innerHTML = Math.round(response.data.wind.speed);
+
+  forecastCoordinates(response.data.coord); //called the forcast function in the weather api function
 }
 
-function searchAPI(cities) {
+function searchAPI(city) {
   let apiKey = "ff69729f5d84be9471536bc122ea91ad";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${cities.value}&appid=${apiKey}&units=metric`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(url).then(weather);
 }
 
 function searchButton(event) {
   event.preventDefault();
   let cities = document.querySelector("#search-input");
-  searchAPI(cities);
+  searchAPI(cities.value.trim());
 }
+
+searchAPI("Nigeria");
 
 function changeDegree(event) {
   event.preventDefault();
@@ -47,60 +108,16 @@ function celsiusDegree(event) {
   myTemp.innerHTML = `${celsius}`;
 }
 
-function weatherForecasts() {
-  let forecastCall = document.querySelector("#weather-forecasts");
-  let forecastContent = `<div class="row">`;
-
-  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  days.forEach(function (day) {
-    forecastContent =
-      forecastContent +
-      `<div class="col-2">
-            <div class="forecast-days">${day}</div>
-              <img src="images/icon.png" class="image-icon" alt="icon" />
-              <div class="forecast-temp">
-                <span class="forecast-temp-max">18℃</span> 
-                <span class="forecast-temp-min">20℃</span>
-            </div>
-      </div>`;
-  });
-
-  forecastContent = forecastContent + `</div>`;
-  forecastCall.innerHTML = forecastContent;
-}
-weatherForecasts();
-
 let celsius = null;
 
 let button = document.querySelector("#search-form");
 button.addEventListener("click", searchButton);
 
-let changeDegrees = document.querySelector("#fahrenheit");
-changeDegrees.addEventListener("click", changeDegree);
+/*let changeDegrees = document.querySelector("#fahrenheit");
+changeDegrees.addEventListener("click", changeDegree);*/
 
 let celsiusDegreeEvent = document.querySelector("#cels");
 celsiusDegreeEvent.addEventListener("click", celsiusDegree);
 
 
-let now = new Date();
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-let day = now.getDay();
-let dayList = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-day = dayList[now.getDay()];
-let h2 = document.querySelector("#date");
-h2.innerHTML = `${day} ${hours}:${minutes}`;
+//weatherForecasts();
